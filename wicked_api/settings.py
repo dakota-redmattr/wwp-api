fe852ecc41d1ded6e3e04b6c3e783a0d5e3ee56a
dakota-redmattr <dakota@redmattr.com>
2020-10-27 13:10:37 -0500

    Another Five

--- wicked_api/settings.py
+++ wicked_api/settings.py
@@ -25,7 +25,7 @@ print("Base Dir: " + f'{BASE_DIR}')
 SECRET_KEY = secure_data()["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
