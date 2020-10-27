016f240fc7f579b80b240a9943d4d5abf0c46474
dakota-redmattr <dakota@redmattr.com>
2020-10-27 11:02:06 -0500

    Another Five

--- wicked_api/settings.py
+++ wicked_api/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = ''
 STATICFILES_DIRS = (os.path.join('static'),)
 
 MEDIA_URL = "/media/images/"
