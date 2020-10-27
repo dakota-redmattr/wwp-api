ba5e50f89371e2c61846e3fed732b1a38433b248
dakota-redmattr <dakota@redmattr.com>
2020-10-27 13:14:03 -0500

    Another Five

--- wicked_api/settings.py
+++ wicked_api/settings.py
@@ -71,7 +71,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'API.secure-data.secure_data'
             ],
         },
     },
